58ea669ae0f391c67a2b3eac6ad24fa66c867d01
satani99 <42287151+satani99@users.noreply.github.com>
2023-10-30 09:57:30 +0530

    Update run.py

--- run.py
+++ run.py
@@ -45,7 +45,7 @@ def run(args):
 
 def parse_args():
     args = argparse.ArgumentParser()
-    args.add_argument('--backend', type=str, choices=['gpt-4', 'gpt-3.5-turbo', 'luna-ai-llama2'], default='luna-ai-llama2')
+    args.add_argument('--backend', type=str, choices=['gpt-4', 'gpt-3.5-turbo', 'luna-ai-llama2', 'llama2-7b', 'llama2-13b', 'zephyr'], default='llama2-7b')
     args.add_argument('--temperature', type=float, default=0.7)
 
     args.add_argument('--task', type=str, required=True, choices=['game24', 'text', 'crosswords'])
