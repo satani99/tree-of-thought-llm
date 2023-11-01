db32672b5d9c22a7a9da6e32cf17d50c473e3f66
satani99 <42287151+satani99@users.noreply.github.com>
2023-11-01 12:22:01 +0530

    Update bfs.sh

--- scripts/game24/bfs.sh
+++ scripts/game24/bfs.sh
@@ -1,10 +1,10 @@
 python run.py \
     --task game24 \
-    --task_start_index 900 \
+    --task_start_index 0 \
     --task_end_index 1000 \
     --method_generate propose \
     --method_evaluate value \
     --method_select greedy \
-    --n_evaluate_sample 3 \
+    --n_evaluate_sample 1 \
     --n_select_sample 5 \
     ${@}
